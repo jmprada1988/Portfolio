691382e52f59eda5ed7a68238424a24c9602c461
davirlein1988 <lein.davir@gmail.com>
2018-10-24 03:45:13 -0500

    added controller portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,43 @@
 class PortfoliosController < ApplicationController
+	def index
+		@portfolio = Portfolio.all
+	end
+
+	def show
+	end
+
+	def new
+		@Portfolio = Portfolio.new
+	end
+
+	def edit
+	end
+
+	def create
+		@Portfolio = Portfolio.new(portfolio_params)
+		respond_to do |format|
+			if @portfolio.save
+				format.html { redirect_to @portfolio, nitice: 'Portfolio was successfully created'}
+			else
+				format.html {render :new}
+			end
+		end
+	end
+
+	def update 
+	end
+
+	def destroy
+	end
+
+	private
+
+	def set_porfolio
+		@portfolio = Portfolio.find(params[:id])
+	end
+
+	def portfolio_params
+		params.require(:portfolio).permit(:title, :body, :main_image, :thumb_image)
+	end
+
 end
