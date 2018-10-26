2d8e64f53fb16b7f92b9ca1fe68edb871320ff3c
davirlein1988 <lein.davir@gmail.com>
2018-10-26 02:14:32 -0500

    added create and new action in porfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,33 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all
 	end
+
+	def new
+		@portfolio_item = Portfolio.new
+	end
+
+	def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Blog was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+
+
+
+
+  private
+
+  def portfolio_params
+  	#do not forget validatins available
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
+
+
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,4 @@
 class Portfolio < ApplicationRecord
-	validates_presence_of :title, :body, :main_image, :thumb_image
+	validates_presence_of :title, :subtitle, :body 
+	# :main_image, :thumb_image
 end
