7a9f3cdc4498fd7bc58899316c921b516a174ab9
davirlein1988 <lein.davir@gmail.com>
2018-10-26 02:31:24 -0500

    integrated edit update in portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,7 +19,20 @@ class PortfoliosController < ApplicationController
     end
   end
 
+def edit
+	@portfolio_item = Portfolio.find(params[:id])
+end
 
+def update
+	@portfolio_item = Portfolio.find(params[:id])
+	respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+end
 
 
 
