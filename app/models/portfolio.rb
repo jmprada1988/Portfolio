7b220c5ec207614b90b431d963a801dd9418642d
davirlein1988 <lein.davir@gmail.com>
2018-10-30 08:54:30 -0500

    implemented defaults fro portfolio images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,13 @@ class Portfolio < ApplicationRecord
 	end
 
 	scope :RoR_items, -> { where(subtitle: 'Ruby on Rails')}
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "https://via.placeholder.com/500?text=Placeholder.com+rocks!"
+		self.thumb_image ||= "https://via.placeholder.com/300?text=Placeholder.com+rocks!"
+	end
 end
+
+
