ae345dadf4ee9d57e1ac1743ef23c083eb50e7c9
davirlein1988 <lein.davir@gmail.com>
2018-11-13 12:47:57 -0500

    implemented custom links from twitter feed in tech-news page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,25 @@
 module PagesHelper
+    def twitter_parser tweet
+        regexp = %r{
+            \b
+            (
+                (?: [a-z][\w-]+:
+                    (?: /{1,3} | [a-z0-9%] ) |
+                    www\d{0,3}[.] |
+                    [a-z0-9.\-]+[.][a-z]{2,4}/
+                )
+                (?:
+                    [^\s()<>]+ | \(([^\s()<>]+|(\([^\s()<>]+\)))*\)
+                )+
+                (?:
+                    \(([^\s()<>]+|(\([^\s()<>]+\)))*\) |
+                    [^\s`!()\[\]{};:'".,<>?\u00AB\u00BB\u201C\u2018\u2019]
+                )
+            )
+        }ix
+
+        tweet.gsub(regexp) do |url|
+            "<a href=#{url} target='_blank'>#{url}</a>"
+        end.html_safe
+    end
 end
