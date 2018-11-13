b3449ddcb5170004b30a9968643c36afc8e18f36
davirlein1988 <lein.davir@gmail.com>
2018-11-12 23:14:27 -0500

    implemented carrierwave with fog-aws

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -1,13 +1,13 @@
 class PortfolioUploader < CarrierWave::Uploader::Base
-  storage :file
+  storage :fog
 
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
   
-  def extension_whitelist
-    %w(jpg jpeg gif png)
-  end
+  # def extension_whitelist
+  #   %w(jpg jpeg gif png)
+  # end
 
  
 end
