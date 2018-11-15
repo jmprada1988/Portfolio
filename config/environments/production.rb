bfc0077a416845cdc8a55c2b46fc1bd3b6cc9a66
davirlein1988 <lein.davir@gmail.com>
2018-11-15 05:27:26 -0500

    implemented production for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://lein-portfolio.herokuapp.com', 'http://lein-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://lein-portfolio.herokuapp.com/cable"
 end
