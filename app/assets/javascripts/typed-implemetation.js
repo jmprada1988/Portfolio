883332f911479c41eb4ced6b0f83eefcf01def60
davirlein1988 <lein.davir@gmail.com>
2018-11-15 22:42:57 -0500

    filled data in home, and about me pages

--- app/assets/javascripts/typed-implemetation.js
+++ app/assets/javascripts/typed-implemetation.js
@@ -2,8 +2,8 @@ $(function() {
   var typed = new Typed(".element", {
     strings: [
       "",
-      "When it comes to software development we should not develop jus for ourselves but for a team. ",
-      " In my projects on top of developing software with best practices I make sure every step taken is properly mentioned."
+      "When it comes to software development we should not develop just for ourselves, but for a team. ",
+      " In my projects, on top of developing software with best practices I make sure every step taken is properly mentioned."
     ],
     typeSpeed: 70
   });
