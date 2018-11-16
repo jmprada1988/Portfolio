13a72d0088edefb43fd6a01e1c5bc76a6e598b7c
davirlein1988 <lein.davir@gmail.com>
2018-11-16 12:34:04 -0500

    changed speed of typewriting

--- app/assets/javascripts/typed-implemetation.js
+++ app/assets/javascripts/typed-implemetation.js
@@ -5,6 +5,6 @@ $(function() {
       "When it comes to software development we should not develop just for ourselves, but for a team. ",
       " In my projects, on top of developing software with best practices I make sure every step taken is properly mentioned."
     ],
-    typeSpeed: 70
+    typeSpeed: 20
   });
 });
